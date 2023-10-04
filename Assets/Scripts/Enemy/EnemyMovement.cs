b478337776de016cfd9bb93ea58c2ac6081015a5
kdu2030 <kdu2030@gmail.com>
2023-10-03 21:24:20 -0400

    Up and Down Animation Working

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -17,19 +17,36 @@ public class EnemyMovement : MonoBehaviour {
         actionHash = Animator.StringToHash("Action");
     }
 
-    private void MoveEnemy() {
+    private Vector3 MoveEnemy() {
         Transform targetWaypoint = EnemyWaypoints.waypoints[waypointIndex];
         Vector3 direction = targetWaypoint.position - transform.position;
         characterController.Move(direction.normalized * enemySpeed * Time.deltaTime);
         if (direction.magnitude <= 0.4f && waypointIndex < EnemyWaypoints.waypoints.Length - 1) {
             waypointIndex++;
         }
+        return direction;
+    }
+
+    private void UpdateAnimation(Vector3 movementDirection) {
+        animator.SetInteger(actionHash, (int)EnemyAction.Walk);
+
+        float horizontalMagnitude = Mathf.Abs(movementDirection.x);
+        float verticalMagnitude = Mathf.Abs(movementDirection.y);
+
+        if (verticalMagnitude > horizontalMagnitude && movementDirection.y < 0) {
+            animator.SetInteger(directionHash, (int)EnemyDirection.Down);
+        }
+        else if (verticalMagnitude > horizontalMagnitude) {
+            animator.SetInteger(directionHash, (int)EnemyDirection.Up);
+        }
+        else {
+            // TODO: Add check for left right and flip animation
+            animator.SetInteger(directionHash, (int)EnemyDirection.Side);
+        }
     }
 
     private void Update() {
-        MoveEnemy();
-        //animator.SetInteger(directionHash, (int)EnemyDirection.Up);
-        //animator.SetInteger(actionHash, (int)EnemyAction.Walk);
-        //animator.SetInteger(directionHash, (int)EnemyDirection.Side);
+        Vector3 updatedEnemyDirection = MoveEnemy();
+        UpdateAnimation(updatedEnemyDirection);
     }
 }
