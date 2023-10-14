de92b0974dd5a4a84d16546685a594e7641b6d87
kdu2030 <kdu2030@gmail.com>
2023-10-14 15:04:23 -0400

    Destroy Bullet Upon Contact with the Enemy

--- Assets/Scripts/Turrents/Bullet.cs
+++ Assets/Scripts/Turrents/Bullet.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
 
 public class Bullet : MonoBehaviour {
+    [Header("References")]
+    [SerializeField] private LayerMask enemyLayer;
+
     [Header("Attributes")]
     [SerializeField] private float bulletSpeed;
 
     private Rigidbody2D rb;
+    private int enemyLayerNum;
     public Transform target { get; set; }
 
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
+        enemyLayerNum = Mathf.RoundToInt(Mathf.Log(enemyLayer.value, 2f));
     }
 
     private void RotateTowardsTarget() {
@@ -24,6 +29,12 @@ public class Bullet : MonoBehaviour {
         if (target) {
             RotateTowardsTarget();
             MoveTowardsTarget();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.layer == enemyLayerNum) {
+            Destroy(gameObject);
         }
     }
 }
