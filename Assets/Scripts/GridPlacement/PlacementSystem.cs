a90a818bbad359e6c0292a4e85cfcfc4b828f3be
kdu2030 <kdu2030@gmail.com>
2023-11-04 17:10:05 -0400

    Fix Cell Indicator Code

--- Assets/Scripts/GridPlacement/PlacementSystem.cs
+++ Assets/Scripts/GridPlacement/PlacementSystem.cs
@@ -22,6 +22,6 @@ public class PlacementSystem : MonoBehaviour {
     private void Update() {
         Vector2 mousePosition = GetMousePosition();
         Vector3Int cellPosition = grid.WorldToCell(mousePosition);
-        cellIndicator.transform.position = grid.CellToWorld(cellPosition);
+        cellIndicator.transform.position = grid.GetCellCenterWorld(cellPosition);
     }
 }
