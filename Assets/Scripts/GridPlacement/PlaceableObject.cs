166d895a9ea65c6b1c8bbfe91fc4e2759a3e6e7f
kdu2030 <kdu2030@gmail.com>
2023-11-26 19:04:58 -0500

    Add Placeable Objects Data

---
+++ Assets/Scripts/GridPlacement/PlaceableObject.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class PlaceableObject {
+    // [field: SerializeField] allows this field to be displayed in the inspector.
+    // {get; private set;} in this context means that the value can only be set in the Inspector
+    [field: SerializeField]
+    public string Name { get; private set; }
+    [field: SerializeField]
+    public int ID { get; private set; }
+    [field: SerializeField]
+    public Vector2Int Size { get; set; } = Vector2Int.one;
+    [field: SerializeField]
+    public GameObject Prefab { get; private set; }
+
+
+}
