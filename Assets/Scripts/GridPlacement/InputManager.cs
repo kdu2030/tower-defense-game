716a4f17f388c3ed4527440df16146d8b7848003
kdu2030 <kdu2030@gmail.com>
2023-12-02 21:42:19 -0500

    Hide and Show Grid Indicator

--- Assets/Scripts/GridPlacement/InputManager.cs
+++ Assets/Scripts/GridPlacement/InputManager.cs
@@ -4,7 +4,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour {
-    public event Action OnClicked, OnExit;
+    //public event Action OnClicked, OnExit;
+    private KeyCode activateBuilderKey = KeyCode.B;
+    public event Action ActivateBuilder;
+
+    private void Update() {
+        if (Input.GetKeyDown(activateBuilderKey)) {
+            ActivateBuilder.Invoke();
+        }
+    }
 
 
 }
--- Assets/Scripts/GridPlacement/PlacementSystem.cs
+++ Assets/Scripts/GridPlacement/PlacementSystem.cs
@@ -4,24 +4,39 @@ public class PlacementSystem : MonoBehaviour {
     [Header("References")]
     [SerializeField] private GameObject cellIndicator;
     [SerializeField] private Grid grid;
+    [SerializeField] private InputManager inputManager;
 
     private SpriteRenderer indicatorSpriteRenderer;
+    private bool isBuilderActive = false;
+
+    private void Awake() {
+        cellIndicator.SetActive(isBuilderActive);
+        indicatorSpriteRenderer = cellIndicator.GetComponent<SpriteRenderer>();
+        Vector2 indicatorDimensions = indicatorSpriteRenderer.bounds.size;
+        cellIndicator.transform.localScale = TransformHelpers.GetScaleFromDimensions(indicatorDimensions, grid.cellSize);
+        inputManager.ActivateBuilder += UpdateBuilderState;
+    }
+
+    private void UpdateBuilderState() {
+        isBuilderActive = !isBuilderActive;
+        cellIndicator.SetActive(isBuilderActive);
+    }
 
     private Vector2 GetMousePosition() {
         Vector3 mousePositionPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         return new Vector2(mousePositionPoint.x, mousePositionPoint.y);
     }
 
-    private void Awake() {
-        indicatorSpriteRenderer = cellIndicator.GetComponent<SpriteRenderer>();
-
-        Vector2 indicatorDimensions = indicatorSpriteRenderer.bounds.size;
-        cellIndicator.transform.localScale = TransformHelpers.GetScaleFromDimensions(indicatorDimensions, grid.cellSize);
-    }
-
-    private void Update() {
+    private void UpdateCellIndicatorPosition() {
         Vector2 mousePosition = GetMousePosition();
         Vector3Int cellPosition = grid.WorldToCell(mousePosition);
         cellIndicator.transform.position = grid.GetCellCenterWorld(cellPosition);
+    }
+
+    private void Update() {
+        if (isBuilderActive) {
+            UpdateCellIndicatorPosition();
+        }
+
     }
 }
