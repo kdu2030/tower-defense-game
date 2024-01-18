4281438b052bdef7dba0efdd2e67a88a94702953
kdu2030 <kdu2030@gmail.com>
2024-01-17 22:01:32 -0500

    Make Indicator appear Red When Hovering over Tree1

--- Assets/Scripts/GridPlacement/CellIndicator.cs
+++ Assets/Scripts/GridPlacement/CellIndicator.cs
@@ -1,14 +1,22 @@
+using System;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CellIndicator : MonoBehaviour {
     [SerializeField] private Grid grid;
+    [SerializeField] private Tilemap pathTilemap;
 
     private SpriteRenderer spriteRenderer;
-    private BoxCollider2D boxCollider;
+    public bool IsValid { get; set; } = true;
+
+    // TODO: Need Script to attach the following to each terrain object
+    /*
+     * 1. Attach Rigidbody2D and Box Collider 2D to each terrain object
+     * 2. Modify the tag to be Terrain
+     */
 
     private void Awake() {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        boxCollider = GetComponent<BoxCollider2D>();
         SetCellIndicatorScale();
     }
 
@@ -17,8 +25,29 @@ public class CellIndicator : MonoBehaviour {
         transform.localScale = TransformHelpers.GetScaleFromDimensions(indicatorDimensions, grid.cellSize);
     }
 
+    public bool CanPlaceObjectOnTile(Vector3 mousePosition) {
+        if (!IsValid) return false;
+
+        Vector3Int pathTilemapCellPosition = pathTilemap.WorldToCell(mousePosition);
+        if (pathTilemap.GetTile(pathTilemapCellPosition) == null) {
+            return true;
+        }
+        return false;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision) {
-        // NOTE: This only gets triggered when another collider enters the sprite collider, so if there is already overlap, it won't trigger again
-        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.tag == TagConstants.Terrain) {
+            IsValid = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.gameObject.tag == TagConstants.Terrain) {
+            IsValid = true;
+        }
+    }
+
+    private void Update() {
+        spriteRenderer.color = IsValid ? Color.white : Color.red;
     }
 }
